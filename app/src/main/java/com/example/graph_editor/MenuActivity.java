fd71a78673b03bf25be7cdb6754c5338ed1e3ed3
aleksanderkatan <aleksanderkatan@student.uj.edu.pl>
2021-10-23 14:19:54 +0200

    added temporary main menu, renamed MainActivity to MenuActivity

---
+++ app/src/main/java/com/example/graph_editor/MenuActivity.java
@@ -0,0 +1,25 @@
+package com.example.graph_editor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+        Button btnBrowse = findViewById(R.id.btnBrowse);
+        btnBrowse.setOnClickListener(v -> {
+            Intent intent = new Intent(this, BrowseActivity.class);
+            startActivity(intent);
+        });
+    }
+}
