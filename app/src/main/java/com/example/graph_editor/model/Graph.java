6b2c6982493d1e35f1f075a0b598f67021d31e48
tymoteusz <tymoteusz.chelmecki@student.uj.edu.pl>
2022-12-11 11:10:57 +0100

    Support properties in Graph objects

--- app/src/main/java/com/example/graph_editor/model/Graph.java
+++ app/src/main/java/com/example/graph_editor/model/Graph.java
@@ -1,10 +1,12 @@
 package com.example.graph_editor.model;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface Graph {
     Vertex addVertex();
     void addEdge(Vertex source, Vertex target);
+    Edge getEdge(Vertex source, Vertex target);
     void removeVertex(Vertex vertex);
     void removeEdge(Edge edge);
     GraphType getType();
@@ -15,4 +17,8 @@ public interface Graph {
     void removeVertexProperty(String name);
     List<Vertex> getVerticesWithProperty(String name);
     List<String> getVertexPropertyNames();
+    void setEdgeProperty(Edge edge, String name, String value);
+    void removeEdgeProperty(String name);
+    List<Edge> getEdgesWithProperty(String name);
+    List<String> getEdgePropertyNames();
 }
