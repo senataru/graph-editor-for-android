8f001892b532130ff7b724d80d9026c5555d2b25
Piotr Kubaty <piotr.kubaty@student.uj.edu.pl>
2023-01-16 21:53:13 +0100

    async server connection

--- app/src/main/java/com/example/graph_editor/extensions/ExtensionsActivity.java
+++ app/src/main/java/com/example/graph_editor/extensions/ExtensionsActivity.java
@@ -2,6 +2,7 @@ package com.example.graph_editor.extensions;
 
 import android.os.Bundle;
 import android.util.Pair;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,11 +12,8 @@ import com.example.graph_editor.client.Client;
 import com.example.graph_editor.model.extensions.ExtensionsClient;
 import com.example.graph_editor.model.extensions.ExtensionsRepository;
 
-import java.io.File;
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 interface OnExtensionInstallClicked {
     void onInstallClicked(String extensionName);
@@ -27,7 +25,7 @@ public class ExtensionsActivity extends AppCompatActivity implements OnExtension
 
     ExtensionsClient client;
 
-    //TODO remove and inject it instead
+    //TODO remove static and inject it instead
     private static ExtensionsRepository installedRepository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,51 +39,11 @@ public class ExtensionsActivity extends AppCompatActivity implements OnExtension
         super.onResume();
         if(installedRepository == null) installedRepository =
                 InstalledExtensionsProvider.getInstance(this.getFilesDir());
-
-        //TODO change to async
-        CompletableFuture<Pair<ExtensionsClient, List<String>>> future =
-                CompletableFuture.supplyAsync(() -> {
-            ExtensionsClient c = new Client("192.168.43.113");
-            try {
-                c.connect();
-                return Pair.create(c, c.getExtensionsList());
-            } catch (Exception e) {
-               return Pair.create(
-                       new ExtensionsClient() {
-                           @Override
-                           public List<String> getExtensionsList() {
-                               return new ArrayList<>();
-                           }
-                           @Override
-                           public void downloadExtension(File root, String name) {}
-                           @Override
-                           public void connect() {}
-                           @Override
-                           public void disconnect() {}
-                       },
-                       new ArrayList<>());
-            }
-        });
-        Pair<ExtensionsClient, List<String>> pair;
-        try {
-            pair = future.get();
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        client = pair.first;
-        List<String> availableExtensions = pair.second;
+        new Thread(() -> tryConnect("192.168.43.113")).start();
 
         installedView.setAdapter(new InstalledExtensionsRecyclerViewAdapter(
                 this,
                 installedRepository.getExtensions()
-                ));
-        availableView.setAdapter(new AvailableExtensionsRecyclerViewAdapter(
-                this,
-                availableExtensions,
-                installedRepository,
-                this
         ));
     }
 
@@ -101,5 +59,27 @@ public class ExtensionsActivity extends AppCompatActivity implements OnExtension
                 e.printStackTrace();
             }
         }).start();
+    }
+    private void tryConnect(String serverAddress) {
+        try {
+            ExtensionsClient c = new Client(serverAddress);
+            c.connect();
+            List<String> availableExtensions = c.getExtensionsList();
+            availableView.post(() -> availableView.setAdapter(new AvailableExtensionsRecyclerViewAdapter(
+                            this,
+                            availableExtensions,
+                            installedRepository,
+                            this
+                            ))
+            );
+        } catch (Exception e) {
+            availableView.post(() -> Toast
+                    .makeText(
+                        this,
+                        "connection to server " + serverAddress  + " failed",
+                        Toast.LENGTH_LONG)
+                    .show()
+            );
+        }
     }
 }
