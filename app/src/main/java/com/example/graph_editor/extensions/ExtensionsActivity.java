c30c81fd87e4993014289f47bac5f73d251c03c8
Piotr Kubaty <piotr.kubaty@student.uj.edu.pl>
2023-01-18 02:27:11 +0100

    allow server customisation

--- app/src/main/java/com/example/graph_editor/extensions/ExtensionsActivity.java
+++ app/src/main/java/com/example/graph_editor/extensions/ExtensionsActivity.java
@@ -1,9 +1,13 @@
 package com.example.graph_editor.extensions;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Pair;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,6 +15,7 @@ import com.example.graph_editor.R;
 import com.example.graph_editor.client.Client;
 import com.example.graph_editor.model.extensions.ExtensionsClient;
 import com.example.graph_editor.model.extensions.ExtensionsRepository;
+import com.example.graph_editor.model.graph_storage.GraphWriter;
 
 import java.time.Duration;
 import java.util.List;
@@ -22,29 +27,48 @@ interface OnExtensionInstallClicked {
 public class ExtensionsActivity extends AppCompatActivity implements OnExtensionInstallClicked {
     RecyclerView installedView;
     RecyclerView availableView;
-
+    EditText ipTextEdit;
     ExtensionsClient client;
+    String ip;
 
     //TODO remove static and inject it instead
     private static ExtensionsRepository installedRepository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(savedInstanceState != null) { ip = savedInstanceState.getString(serverIpKey); };
         setContentView(R.layout.activity_extensions);
         installedView = findViewById(R.id.installedExtensions);
         availableView = findViewById(R.id.availableExtensions);
+        ipTextEdit = findViewById(R.id.serverAddress);
+        ipTextEdit.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                ip = s.toString();
+                tryConnectAsync();
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+        });
     }
     @Override
     protected void onResume() {
         super.onResume();
         if(installedRepository == null) installedRepository =
                 InstalledExtensionsProvider.getInstance(this.getFilesDir());
-        new Thread(() -> tryConnect("192.168.43.113")).start();
+        ip = ipTextEdit.getText().toString();
+        tryConnectAsync();
 
         installedView.setAdapter(new InstalledExtensionsRecyclerViewAdapter(
                 this,
                 installedRepository.getExtensions()
         ));
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(serverIpKey, ip);
     }
 
     // TODO move to presenter
@@ -60,26 +84,28 @@ public class ExtensionsActivity extends AppCompatActivity implements OnExtension
             }
         }).start();
     }
-    private void tryConnect(String serverAddress) {
-        try {
-            ExtensionsClient c = new Client(serverAddress);
-            c.connect();
-            List<String> availableExtensions = c.getExtensionsList();
-            availableView.post(() -> availableView.setAdapter(new AvailableExtensionsRecyclerViewAdapter(
-                            this,
-                            availableExtensions,
-                            installedRepository,
-                            this
-                            ))
-            );
-        } catch (Exception e) {
-            availableView.post(() -> Toast
-                    .makeText(
-                        this,
-                        "connection to server " + serverAddress  + " failed",
-                        Toast.LENGTH_LONG)
-                    .show()
-            );
-        }
+    private void tryConnectAsync() {
+        new Thread(() -> {
+            try {
+                client = new Client(ip);
+                client.connect();
+                List<String> availableExtensions = client.getExtensionsList();
+                availableView.post(() -> availableView.setAdapter(new AvailableExtensionsRecyclerViewAdapter(
+                                this,
+                                availableExtensions,
+                                installedRepository,
+                                this
+                        ))
+                );
+            } catch (Exception e) {
+                availableView.post(() -> {
+                            Toast.makeText(this, "connection to server " + ip  + " failed", Toast.LENGTH_LONG).show();
+                            Toast.makeText(this, e.getClass().toString(), Toast.LENGTH_LONG).show();
+                        }
+                );
+            }
+        }).start();
     }
+
+    private static String serverIpKey = "serverIp";
 }
