58e9d4bcf70c7748d757ce2f58db0988d1509382
Piotr Kubaty <piotr.kubaty@student.uj.edu.pl>
2023-02-05 02:22:27 +0100

    cleanup

--- app/src/main/java/com/example/graph_editor/model/PropertyContainer.java
+++
@@ -1,7 +0,0 @@
-package com.example.graph_editor.model;
-
-public interface PropertyContainer {
-    String getProperty(String name);
-    void setProperty(String name, String value);
-    void removeProperty(String name);
-}
