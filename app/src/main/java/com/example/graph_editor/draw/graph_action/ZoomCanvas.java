2f23ddd87c323a65cf1d3d1f8eeab093ff8ca6c9
Piotr Kubaty <piotr.kubaty@student.uj.edu.pl>
2023-02-05 23:05:22 +0100

    cleanup

--- app/src/main/java/com/example/graph_editor/draw/graph_action/ZoomCanvas.java
+++ app/src/main/java/com/example/graph_editor/draw/graph_action/ZoomCanvas.java
@@ -29,32 +29,3 @@ public class ZoomCanvas implements GraphAction {
         return stack.getCurrent();
     }
 }
-//    class ZoomCanvas implements GraphAction {
-//        @Override
-//        public boolean perform(GraphView view, @NonNull MotionEvent event, VersionStack<GraphVisualization<PropertySupportingGraph>> stack, GraphOnTouchMutation mutation) {
-//            int indexA = event.findPointerIndex(data.firstPointerId);
-//            int indexB = event.findPointerIndex(data.secondPointerId);
-//
-//            switch (event.getAction() & MotionEvent.ACTION_MASK) {
-//                case MotionEvent.ACTION_MOVE:
-//                    if (indexA == -1 || indexB == -1) data.zoomIsDeactivated = true;
-//                    if (data.zoomIsDeactivated) break;
-//
-//                    Point firstRelativeEnd = view.getRelative(new Point(event.getX(indexA), event.getY(indexA)));
-//                    Point secondRelativeEnd = view.getRelative(new Point(event.getX(indexB), event.getY(indexB)));
-//
-//                    Rectangle newRectangle = DrawManager.getZoomedRectangle(data.zoomInitialRectangle, data.zoomFirstAbsoluteStart, data.zoomSecondAbsoluteStart, firstRelativeEnd, secondRelativeEnd);
-//                    stack.getCurrentState().setRectangle(newRectangle);
-//
-//                    break;
-//                case MotionEvent.ACTION_POINTER_DOWN:
-//                case MotionEvent.ACTION_POINTER_UP:
-//                    data.zoomIsDeactivated = true;
-//                    break;
-//                case MotionEvent.ACTION_UP:
-//                    stack.getCurrentState().setGraphAction(new MoveCanvas());
-//                    break;
-//            }
-//            return true;
-//        }
-//    }
