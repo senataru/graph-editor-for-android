e12995505c1fd984a4bd092c5288284da77eadaf
Piotr Kubaty <piotr.kubaty@student.uj.edu.pl>
2023-02-12 03:50:04 +0100

    introduce using of core's publication to mavenlocal

--- app/src/main/java/com/example/graph_editor/draw/ExtensionsMenuOptions.java
+++ app/src/main/java/com/example/graph_editor/draw/ExtensionsMenuOptions.java
@@ -2,8 +2,8 @@ package com.example.graph_editor.draw;
 
 import com.example.graph_editor.extensions.GraphMenuManager;
 
-import java.util.Map;
 
 public class ExtensionsMenuOptions {
     public static Map<Integer, GraphMenuManager.MenuOptionHandler> extensionsOptions;
 }
+
