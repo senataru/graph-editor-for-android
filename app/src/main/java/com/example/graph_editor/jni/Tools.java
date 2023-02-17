3e45d5b2e2b76ab10424764227911d319f734e34
dandrozavr <andrei.daletski@student.uj.edu.pl>
2023-02-17 13:34:31 +0100

    Added native methods

--- app/src/main/java/com/example/graph_editor/jni/Tools.java
+++ app/src/main/java/com/example/graph_editor/jni/Tools.java
@@ -2,4 +2,6 @@ package com.example.graph_editor.jni;
 
 public class Tools {
     public native double[] arrange(int n, int m, double[] x, double[] y, int[] tab_edge_source, int[] tab_edge_target);
+    public native double[] arrangePlanar(int n, int m, double[] x, double[] y, int[] tab_edge_source, int[] tab_edge_target);
+    public native double[] makePlanar(int n, int m, double[] x, double[] y, int[] tab_edge_source, int[] tab_edge_target);
 }
